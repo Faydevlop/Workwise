269ff53e67b85a4ec8e179f174134210861e7198
fayis <fayfayis74@gmail.com>
2024-09-26 10:18:45 +0530

    user listing issue fix

--- src/pages/admin/Usermanagment.jsx
+++ src/pages/admin/Usermanagment.jsx
@@ -73,7 +73,7 @@ const handleDelete = async (userId) =>{
     useEffect(()=>{
         const fetchUser = async () =>{
             try {
-                const response = await api.get(`/admin/getusers`,{
+                const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/admin/getusers`,{
                   headers:{
                     Authorization:`Bearer ${localStorage.getItem('token')}`,
                   }
